ca8ce219bf5f0253bfa2be7637abfba66ccc1bf8
Andrew Reid East <andrew@andreweast.net>
2017-11-02 15:42:59 +0000

    Finish question 2

--- q2.pl
+++ q2.pl
@@ -3,9 +3,10 @@ takes(mary, ct331).
 takes(joe, ct331).
 takes(tom, ct345).
 takes(mary, ct345).
+takes(jack, ct345).
 instructs(bob, ct331).
 instructs(ann, ct345).
 
-teaches(INSTRUCTOR, STUDENT) :- instructs(INSTRUCTOR, CLASS),takes(STUDENT, CLASS).
+teaches(INSTRUCTOR, STUDENT) :- instructs(INSTRUCTOR, CLASS), takes(STUDENT, CLASS).
 
-
+classmates(A, B) :- takes(A, CLASS), takes(B, CLASS).
