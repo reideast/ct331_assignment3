8114ba42603136289a825024f8f5d8d1b9d13d60
Andrew Reid East <andrew@andreweast.net>
2017-11-02 15:12:00 +0000

    Queries for Q2

---
+++ q2.pl
@@ -0,0 +1,11 @@
+takes(tom, ct331).
+takes(mary, ct331).
+takes(joe, ct331).
+takes(tom, ct345).
+takes(mary, ct345).
+instructs(bob, ct331).
+instructs(ann, ct345).
+
+teaches(INSTRUCTOR, STUDENT) :- instructs(INSTRUCTOR, CLASS),takes(STUDENT, CLASS).
+
+
